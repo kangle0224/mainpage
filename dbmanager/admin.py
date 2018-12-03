c6aeee4a6ced273b09c84ebd3b5ad09480814211
kangle0224 <kangle0224@gmail.com>
2018-12-03 21:15:04 +0800

    first commit

---
+++ dbmanager/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from dbmanager.models import UserInfo, Course
+
+admin.site.register(UserInfo)
+admin.site.register(Course)
---
+++ dbmanager/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class UserInfo(models.Model):
+    name = models.CharField(u'用户名', max_length=50)
+    age = models.IntegerField(u'年龄')
+    email = models.EmailField(u'邮箱')
+    user_type_choice = (
+        ('0', '校长'),
+        ('1', '教师'),
+        ('2', '学生'),
+    )
+    user_type = models.CharField(choices=user_type_choice, max_length=10, default=2) # 此处是字符字段，user_type_choice也必须是字符
+
+    def __str__(self):
+        return self.name
+
+    class META:
+        __table_name__ = 'userinfo'
+
+class Course(models.Model):
+    name = models.CharField(u'课程名', max_length=50)
+    price = models.CharField(u'价格', max_length=50)
+    teacher = models.ForeignKey(UserInfo, on_delete=False)
+    def __str__(self):
+        return self.name
+
+    class META:
+        __table_name__ = 'course_name'
+
+
+
+
